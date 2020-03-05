7ece6872b1d98d56809a1bf7479aa8b3b565d171
Nathan Shostek <szos@posteo.net>
2020-03-05 16:10:45 +0100

    refactored option functions

--- clfm-variables.lisp
+++ clfm-variables.lisp
@@ -2,33 +2,31 @@
 (in-package :clfm)
 
 (defparameter *default-text-style-fallback*
-  (make-text-style :sans-serif :roman :normal))
+  (make-text-style :serif :roman :normal))
 
 (defparameter *options*
   '((allow-xdg-open . t)
     (prefer-xdg-open . nil)
-    (hide-hidden-files . t)))
+    (hide-hidden-files . t)
+    (display-group . t)
+    (display-user . t)
+    (display-permissions . t)))
 
-(defun prefer-xdg-open? ()
-  (cdr (assoc 'prefer-xdg-open *options*)))
-
-(defmethod (setf prefer-xdg-open?) (new-val)
-  (let ((var (assoc 'prefer-xdg-open *options*)))
-    (setf (cdr var) new-val)))
-
-(defun hide-files? ()
-  (cdr (assoc 'hide-hidden-files *options*)))
-
-(defmethod (setf hide-files?) (new-val)
-  (let ((hide (assoc 'hide-hidden-files *options*)))
-    (setf (cdr hide) new-val)))
-
-(defun allow-xdg-open? ()
-  (cdr (assoc 'allow-xdg-open *options*)))
-
-(defmethod (setf allow-xdg-open?) (new-val)
-  (let ((var (assoc 'allow-xdg-open *options*)))
-    (setf (cdr var) new-val)))
+(macrolet ((make-option-functions (name option-name)
+	     (let ((value (gensym))
+		   (var (gensym)))
+	       `(progn
+		  (defun ,name ()
+		    (cdr (assoc ',option-name *options*)))
+		  (defmethod (setf ,name) (,value)
+		    (let ((,var (assoc ',option-name *options*)))
+		      (setf (cdr ,var) ,value)))))))
+  (make-option-functions display-permissions? display-permissions)
+  (make-option-functions display-group? display-group)
+  (make-option-functions display-user? display-user)
+  (make-option-functions prefer-xdg-open? prefer-xdg-open)
+  (make-option-functions hide-files? hide-hidden-files)
+  (make-option-functions allow-xdg-open? allow-xdg-open))
 
 (defparameter *uid-list* '())
 
